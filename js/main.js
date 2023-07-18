776e758839653d21be53ee01b531bca878a2f101
Griffin Overmyer <griffinovermyer@gmail.com>
2023-07-17 20:39:45 -0400

    form validation and working nav links

--- js/main.js
+++ js/main.js
@@ -4,6 +4,12 @@ const carouselItems = document.querySelectorAll(".caro-item");
 
 const KnowledgeBtns = document.querySelectorAll(".flex-item");
 
+const contactForm = document.querySelector("#contact-form");
+const [nameInput, emailInput, bodyInput] =
+  contactForm.querySelectorAll("input, textarea");
+const [nameValidation, emailValidation, bodyValidation] =
+  contactForm.querySelectorAll(".validation");
+
 let screenWidth;
 let maxIndex;
 //resets the max viewable items in carosel
@@ -62,4 +68,40 @@ KnowledgeBtns.forEach((el) => {
     const content = this.children[2];
     content.classList.toggle("open");
   });
+});
+
+//contact form
+contactForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  let errorFlag = false;
+
+  if (nameInput.value.trim() == "") {
+    nameValidation.textContent = "Name is required";
+    errorFlag = true;
+  } else {
+    nameValidation.textContent = "";
+  }
+
+  const emailVal = emailInput.value.trim();
+  if (
+    !emailVal.match(
+      /(?:[a-z0-9+!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/gi
+    )
+  ) {
+    emailValidation.textContent = "Proper email format is required";
+    errorFlag = true;
+  } else {
+    emailValidation.textContent = "";
+  }
+
+  if (bodyInput.value.trim() == "") {
+    bodyValidation.textContent = "body is required";
+    errorFlag = true;
+  } else {
+    bodyValidation.textContent = "";
+  }
+
+  if (!errorFlag) {
+    this.submit();
+  }
 });
